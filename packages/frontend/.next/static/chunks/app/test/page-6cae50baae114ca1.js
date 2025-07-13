226a5c7fb1971d4c2f6ff36893e51187f3e2e9f4
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2025-07-13 12:30:31 +0530

    fix: resolve all build issues and add environment variables

---
+++ packages/frontend/.next/static/chunks/app/test/page-6cae50baae114ca1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[11],{78567:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[927,518,358],()=>e(78567)),_N_E=_.O()}]);
---
+++ packages/frontend/.next/static/chunks/pages/_app-b0629b4c08104108.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6180:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(45524)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6180),n(754))),_N_E=_.O()}]);
